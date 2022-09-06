6e444fa1c635fd0b51c03526625545b34a72108f
hicso <tv21102000@gmail.com>
2022-09-06 14:07:39 -0300

    pusheo de react I

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      
+
+      <Routes>
+        <Route path='/user' element={''}/>
+        <Route path='/pets' element={''}/>
+        <Route path='/pet-profile' element={''}/>
+        <Route path='/fundations' element={''}/>
+        <Route path='/fundation-profile' element={''}/>
+        <Route path='/login' element={''}/>
+        <Route path='/register' element={''}/>
+      </Routes>
     </>
   );
 }
