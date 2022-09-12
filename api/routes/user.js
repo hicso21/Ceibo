8f3f9d1e4e3f3796b604ab7884f0d44f18e2f9d0
AleEspindola <ale.espindola@outlook.com>
2022-09-12 16:57:03 -0300

    agregando ruta para modificar mascota

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/user.controllers");
 
-router.post('/register/', UserController.createUser)
+router.post("/register/", UserController.createUser);
 
 module.exports = router;
