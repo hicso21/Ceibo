8827cfbc1d42cbe73cab7b712d001baaa9f191e9
hicso <tv21102000@gmail.com>
2022-09-13 17:46:06 -0300

    pusheo de persistencia parte 3

---
+++ api/routes/user.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const UserController = require("../controllers/user.controllers");
+const { validateAuth } = require('../middlewares/authUser')
+
+
+
+router.post('/register', UserController.createUser)
+
+router.post('/login', UserController.logIn)
+
+router.get('/me', validateAuth, (req, res) => {console.log(req.user); res.send(req.user)})
+
+router.get('/:id', UserController.getUser)
+
+router.post('/logout', UserController.logOut)
+
+router.delete('/delete/:id', UserController.deleteUser)
+
+router.put('/update/', UserController.userUpdate)
+
+
+
+
+module.exports = router;
